348583387e81270b2deaa999f8db24329b8d74ac
ZaHiD-HaSaN-72 <jahidhasan19369@gmail.com>
2023-09-21 13:59:44 +0600

    add

---
+++ script-2.js
@@ -0,0 +1,24 @@
+
+for ( let i =1; i<=10; i++){
+    for(let t =1; t<=10; t++){
+
+        console.log( i+" * "+ t + " = "+ t*i)
+    }
+}
+// even odd
+for ( let i =1; i<=10; i++){
+    for(let t =1; t<=10; t++){
+        if(t %2==0){
+            
+            console.log( i," * ", t , " = ", t*i ,"<br/>");
+        }
+        else{
+            console.log('The number is odd');
+        }
+        
+    }
+}
+// even odd
+
+
+
